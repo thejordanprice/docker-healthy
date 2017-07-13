d1e9e33c2f5b121ae1ba1e70773014e6fdabeb15
Jordan Price <thejordanprice@gmx.com>
2017-07-13 03:58:18 -0400

    Update healthy.sh

--- healthy.sh
+++ healthy.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 PATH=/usr/bin:/bin
 
-## CONTAINER CHECKS
+function Checks {
+  ## CONTAINER CHECKS
 
-# nginx-balancer
-if [[ $(docker ps -af name=nginx-balancer | grep Exited) != null ]];
-then
-    docker start nginx-balancer > /dev/null;
-fi
+  # nginx-balancer
+  # this run docker and sees if the vm named 'nginx-balancer' is running.
+  # it will return null if it is not running.
+  # if it was null, start it.
+  if [[ $(docker ps -af name=nginx-balancer | grep Exited) != null ]];
+  then
+      docker start nginx-balancer > /dev/null;
+  fi
 
-## INDIVIDUAL SERVICES
+  ## INDIVIDUAL SERVICES
 
-# nginx-balancer nginx
-if [[ $(docker exec -i nginx-balancer pgrep nginx | wc -l) < 1 ]];
-then
-    docker exec -i nginx-balancer service nginx start;
-fi
+  # nginx-balancer nginx
+  # we can execute pgrep inside the vm to see if the individual services are running.
+  # if not we'll start them.
+  if [[ $(docker exec -i nginx-balancer pgrep nginx | wc -l) < 1 ]];
+  then
+      docker exec -i nginx-balancer service nginx start;
+  fi
+}
+
+Checks;
